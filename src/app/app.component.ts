5734c69dc6db478652b01350ad0d3bd7e9987c92
tylerastraub <tylerandstraub@gmail.com>
2024-06-01 18:52:44 -0600

    Changed sidebar to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { RouterOutlet, provideRouter } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appRoutes } from './shared/routes';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [
         CommonModule,
-        SidebarComponent,
+        NavbarComponent,
         RouterOutlet,
     ],
     templateUrl: './app.component.html',
